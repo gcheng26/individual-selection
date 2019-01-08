ee335c91823c6802d7a0accf67e10ae95c6eddc9
Geoffrey <gcsf1c15@soton.ac.uk>
2019-01-08 09:35:24 +0000

    Fully tested

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
+# Author: Geoffrey Cheng Sang Fong
+# A Niche construction algorithm to investigate Individual Selection for Cooperative Group Formation
+# E-mail: gcsf1c15@soton.ac.uk
+# GitHub repo: https://github.com/gcheng26/individual-selection
+
 from toolbox import start_seq
 
 start_seq()
--- toolbox.py
+++ toolbox.py
@@ -1,6 +1,14 @@
+# Author: Geoffrey Cheng Sang Fong
+# A Niche construction algorithm to investigate Individual Selection for Cooperative Group Formation
+# E-mail: gcsf1c15@soton.ac.uk
+# GitHub repo: https://github.com/gcheng26/individual-selection
+
+
 import numpy as np
 import matplotlib.pyplot as plt
-from random import randrange
+from random import randrange, shuffle
+
+# if you are reading this, I apologise for the agony you're about to put your eyes through...
 
 G_coop = 0.018
 G_selfish = 0.02
@@ -12,6 +20,9 @@ K = 0.1
 Res_s = 4
 Res_l = 50
 t = 4
+mutation_rate = 0.2
+res_option = 'constant'
+mutation_bias = 0.2
 
 
 class SmallGroup:
@@ -35,22 +46,48 @@ class SmallGroup:
             self.prop[member] += 1
             self.initial_seed[member] += 1
 
-    def calc_r(self):
-        if self.type == 'small':
-            self.share[0] = (self.prop[0] * G_coop * C_coop * Res_s) / \
-                            ((self.prop[0] * G_coop * C_coop) + (self.prop[1] * G_selfish * C_selfish))
-            self.share[1] = Res_s - self.share[0]
+    def calc_r(self, option='constant'):
+        if option == 'constant':
+            if self.type == 'small':
+                self.share[0] = (self.prop[0] * G_coop * C_coop * Res_s) / \
+                                ((self.prop[0] * G_coop * C_coop) + (self.prop[1] * G_selfish * C_selfish))
+                self.share[1] = Res_s - self.share[0]
 
+            else:
+                raise Exception('Type error! - Should be Small but is {}' .format(self.type))
+
+        elif option == 'random':
+            res = randrange(1, 6)
+            if self.type == 'small':
+                self.share[0] = (self.prop[0] * G_coop * C_coop * res) / \
+                                ((self.prop[0] * G_coop * C_coop) + (self.prop[1] * G_selfish * C_selfish))
+                self.share[1] = res - self.share[0]
+
+            else:
+                raise Exception('Type error! - Should be Small but is {}'.format(self.type))
         else:
-            raise Exception('Type error! - Should be Small but is {}' .format(self.type))
+            raise Exception('Option error! - Should be constant or random but is {}'.format(option))
+
+    def mutation(self, rate=0, bias=0):
+        if bias is None and rate is None:
+            mutants = [0, 0]
+        elif bias is None and rate is not None:
+            mutants = [int(self.prop[0] * rate), int(self.prop[1] * rate)]
+        elif rate is None and bias is not None:
+            mutants = [int(self.prop[0] * bias), int(self.prop[1] * (1 - bias))]
+        else:
+            mutants = [int(self.prop[0] * rate * bias), int(self.prop[1] * rate * (1 - bias))]
+        return mutants
 
     def update_pop(self):
         for i in range(t):
-            self.calc_r()
+            self.calc_r(option=res_option)
             # initial + births + deaths of cooperative
             self.prop[0] = self.prop[0] + int(self.share[0] / C_coop) - int(K * self.prop[0])
             # initial + births - deaths of selfish
             self.prop[1] = self.prop[1] + int(self.share[1] / C_selfish) - int(K * self.prop[1])
+            temp = self.mutation(rate=mutation_rate, bias=mutation_bias)
+            self.prop = [self.prop[0] + temp[1] - temp[0], self.prop[1] + temp[0] - temp[1]]
 
     def get_progeny(self):
         return np.subtract(self.prop, self.initial_seed)
@@ -88,22 +125,49 @@ class LargeGroup:
             self.prop[member-2] += 1
             self.initial_seed[member-2] += 1
 
-    def calc_r(self):
-        if self.type == 'large':
-            self.share[0] = (self.prop[0] * G_coop * C_coop * Res_l) / \
-                            ((self.prop[0] * G_coop * C_coop) + (self.prop[1] * G_selfish * C_selfish))
-            self.share[1] = Res_l - self.share[0]
+    def calc_r(self, option='constant'):
+        if option == 'constant':
+            if self.type == 'large':
+                self.share[0] = (self.prop[0] * G_coop * C_coop * Res_l) / \
+                                ((self.prop[0] * G_coop * C_coop) + (self.prop[1] * G_selfish * C_selfish))
+                self.share[1] = Res_l - self.share[0]
+
+            else:
+                raise Exception('Type error! - Should be Large but is {}' .format(self.type))
+
+        elif option == 'random':
+            res = randrange(40, 61)
+            if self.type == 'large':
+                self.share[0] = (self.prop[0] * G_coop * C_coop * res) / \
+                                ((self.prop[0] * G_coop * C_coop) + (self.prop[1] * G_selfish * C_selfish))
+                self.share[1] = res - self.share[0]
+
+            else:
+                raise Exception('Type error! - Should be Large but is {}'.format(self.type))
 
         else:
-            raise Exception('Type error! - Should be Large but is {}' .format(self.type))
+            raise Exception('Option error! - Should be constant or random but is {}'.format(option))
+
+    def mutation(self, rate=0, bias=None):
+        if bias is None and rate is None:
+            mutants = [0, 0]
+        elif bias is None and rate is not None:
+            mutants = [int(self.prop[0] * rate), int(self.prop[1] * rate)]
+        elif rate is None and bias is not None:
+            mutants = [int(self.prop[0] * bias), int(self.prop[1] * (1 - bias))]
+        else:
+            mutants = [int(self.prop[0] * rate * bias), int(self.prop[1] * rate * (1 - bias))]
+        return mutants
 
     def update_pop(self):
         for i in range(t):
-            self.calc_r()
+            self.calc_r(option=res_option)
             # initial + births + deaths of cooperative
             self.prop[0] = self.prop[0] + int(self.share[0] / C_coop) - int(K * self.prop[0])
             # initial + births - deaths of selfish
             self.prop[1] = self.prop[1] + int(self.share[1] / C_selfish) - int(K * self.prop[1])
+            temp = self.mutation(rate=mutation_rate, bias=mutation_bias)
+            self.prop = [self.prop[0] + temp[1] - temp[0], self.prop[1] + temp[0] - temp[1]]
 
     def get_progeny(self):
         # print('prop')
@@ -136,6 +200,14 @@ class MigrantPool(SmallGroup, LargeGroup):
         for i in range(N):
             self.migrants.append(i % 4)
 
+        print(self.migrants)
+
+    def custom_first(self, proportions):
+        for j in range(len(proportions)):
+            for i in range(proportions[j]):
+                self.migrants.append(j)
+
+        shuffle(self.migrants)
         print(self.migrants)
 
     def census(self):
@@ -213,7 +285,7 @@ def start_seq():
     prop_arr = []
     gen = [0]
     migrant_pool = MigrantPool()
-    migrant_pool.first()
+    migrant_pool.custom_first([1000, 1000, 1000, 1000])
     migrant_pool.census()
     prop_arr.append(migrant_pool.prop)
     for i in range(T):
@@ -230,6 +302,10 @@ def start_seq():
     s_s = [i[1] for i in prop_arr]
     l_c = [i[2] for i in prop_arr]
     l_s = [i[3] for i in prop_arr]
+    large = np.add(l_c, l_s)
+    selfish = np.add(s_s, l_s)
+    plt.figure(1)
+    plt.subplot(221)
     plt.plot(gen, s_c, '.', label='small + cooperative')
     plt.plot(gen, s_s, '-.', label='small + selfish')
     plt.plot(gen, l_c, ':', label='large + cooperative ')
@@ -237,7 +313,11 @@ def start_seq():
     plt.xlabel('Generations')
     plt.ylabel('Frequecy')
     plt.legend()
+    plt.subplot(222)
+    plt.plot(gen, large, '.', label='large')
+    plt.plot(gen, selfish, '-', label='selfish')
+    plt.xlabel('Generations')
+    plt.ylabel('Frequecy')
+    plt.legend()
     plt.show()
-
-
 
